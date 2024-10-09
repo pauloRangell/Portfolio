58ebf3dc3c9cfd3df3c0115ba4b96eefa5e70017
pauloRangel <112969557+pauloRangell@users.noreply.github.com>
2024-10-08 22:25:49 -0300

    completes Bin2Dec project.

--- Bin2Dec/main.py
+++ Bin2Dec/main.py
@@ -1,40 +1,40 @@
 import flet as ft
-def is_a_valid_binary_number(input:str):
-    if len(input) > 8:
-        raise ValueError("input must be a binary number of 8 digits")
-    if not all(map(lambda digit: digit in ["0","1"], input)):
-        raise ValueError("It must contain just 1's and 0's")
-    
-def binary_to_decimal(x:str):
-    r = 0
-    for i, n in enumerate(x):
-        r += int(n) * 2 ** ((len(x) - 1) - i)
-    return r
+from itertools import starmap
 
-
-
-
-
-
-
+output = ft.Text("", size=24)
 
 def main(page: ft.Page):
+    
     page.title = "Bin2Dec"
     page.theme_mode = "light"
     user_input = ft.TextField(label="Enter a binary number of 8 digits", width=400)
-    decimal_representation = None
+    
+    
+    def binary_to_decimal(input:str):
+        input_is_not_up_to_8_digits = len(input) > 8
+        input_is_not_binary = not all(map(lambda digit: digit in ["0","1"], input))
+        global output
+        
+        if input_is_not_up_to_8_digits:
+            output.value = "Binary number must e up to 8 digits!"
+        elif input_is_not_binary:
+            output.value = "Binary numbers are composed just by 1's and 0's."
+        else:
+            decimal_form = sum(starmap(lambda index, digit: int(digit) * 2 ** ((len(input) - 1) - index),enumerate(input[::-1])))
+            output.value = f"This binary number in decimal is {decimal_form}"
+        page.update()
+        
+        
     page.add(
         
         ft.Container(
             content=ft.Column(controls=[
                 user_input,
-                ft.ElevatedButton(text="Convert to decimal"), # add on_click argument
-                ft.Text(value=decimal_representation)
+                ft.ElevatedButton(text="Convert to decimal", on_click= lambda e: binary_to_decimal(user_input.value)),
+                output
             ],
             alignment="center",horizontal_alignment="center", expand=True),
             alignment=ft.alignment.center, expand=True)
-   
-    
 )
 
 ft.app(main)
