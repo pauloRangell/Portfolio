54e275fd72c4ed3ff68d01395b58613e2374aa6e
pauloRangel <112969557+pauloRangell@users.noreply.github.com>
2024-10-12 15:13:40 -0300

    Creates project's bone (html, css and js )

---
+++ Border-radius Previewer/index.js
@@ -0,0 +1,7 @@
+inputBorderTopLeft = document.querySelector(".border-top-left");
+inputBorderTopRight = document.querySelector(".border-top-right");
+inputBorderBottomLeft = document.querySelector(".border-bottom-left");
+inputBorderBottomRight = document.querySelector(".border-bottom-right");
+previewBlock = document.querySelector(".preview-block");
+
+
