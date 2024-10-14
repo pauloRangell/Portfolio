fbfa57a8089fc8dfaef929780e26e1211eaafcf9
pauloRangel <112969557+pauloRangell@users.noreply.github.com>
2024-10-13 23:13:34 -0300

    Starts updateBorderRadius function.

--- Border-radius Previewer/index.js
+++ Border-radius Previewer/index.js
@@ -4,4 +4,9 @@ inputBorderBottomLeft = document.querySelector(".border-bottom-left");
 inputBorderBottomRight = document.querySelector(".border-bottom-right");
 previewBlock = document.querySelector(".preview-block");
 
+function updateBorderRadius(event){
+    new_value_to_border_radius = event.target.value;
+    
+};
+
 
