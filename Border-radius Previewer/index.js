5616c9ac85c37b7858ef5c5a69960a53daedd429
pauloRangel <112969557+pauloRangell@users.noreply.github.com>
2024-10-25 10:28:35 -0300

    "Ends Border-radius previewer project"

--- Border-radius Previewer/index.js
+++ Border-radius Previewer/index.js
@@ -1,12 +1,44 @@
-inputBorderTopLeft = document.querySelector(".border-top-left");
-inputBorderTopRight = document.querySelector(".border-top-right");
-inputBorderBottomLeft = document.querySelector(".border-bottom-left");
-inputBorderBottomRight = document.querySelector(".border-bottom-right");
+inputBTL = document.querySelector(".border-top-left");
+inputBTR = document.querySelector(".border-top-right");
+inputBBL = document.querySelector(".border-bottom-left");
+inputBBR = document.querySelector(".border-bottom-right");
 previewBlock = document.querySelector(".preview-block");
+resetButtom = document.querySelector(".text-buttom");
+copyButtom = document.querySelectorAll("span.text-buttom")[1]
 
-function updateBorderRadius(event){
-    new_value_to_border_radius = event.target.value;
-    
+
+updateBorderRadius = (event) => {
+    isEmpty = (input) => {
+        return input.value === "" ? 0 : input.value
+    };
+    previewBlock.style.cssText = `border-radius: ${isEmpty(inputBTL)}px ${isEmpty(inputBTR)}px ${isEmpty(inputBBR)}px ${isEmpty(inputBBL)}px;`;
 };
+
+resetBorderRadius = (event) => {
+    if (event.target === resetButtom){
+        inputBBL.value = "";
+        inputBBR.value = "";
+        inputBTL.value = "";
+        inputBTR.value = "";
+        previewBlock.style.cssText = "border-radius: 0px 0px 0px 0px;" ;
+    }
+
+}
+
+copyBorderRadius = (event) => {
+    if (event.target == copyButtom){
+        const borderRadiusValue = window.getComputedStyle(previewBlock).getPropertyValue('border-radius');
+        navigator.clipboard.writeText(borderRadiusValue);
+        alert("Border radius value copied!")
+    }
+}
+
+
+document.addEventListener("input", updateBorderRadius);
+document.addEventListener("click", resetBorderRadius);
+document.addEventListener("click", copyBorderRadius);
+
+
+
 
 
